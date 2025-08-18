11f5893194e5b6649eb37a3c3a8a04d86a70a9d5
Juergen Hock <juergen.hock@iosb.fraunhofer.de>
2025-08-18 17:34:08 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set requirements=build
 uv venv --allow-existing
 uv pip install %requirements%
 
-uv run python -m build --wheel --outdir dist --no-isolation .
-uv run python -m build --wheel --outdir dist --no-isolation ./src/pycat
-uv run python -m build --wheel --outdir dist --no-isolation ./src/pydog
-uv run python -m build --wheel --outdir dist --no-isolation ./src/pypet
+uv run python -m build --wheel --no-isolation --outdir dist .
+uv run python -m build --wheel --no-isolation --outdir dist ./src/pycat
+uv run python -m build --wheel --no-isolation --outdir dist ./src/pydog
+uv run python -m build --wheel --no-isolation --outdir dist ./src/pypet
